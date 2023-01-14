39d79f5524cb2b8793db879dfb44517eaf7c1be0
MaksymilianBilski <biger9@o2.pl>
2023-01-14 11:20:43 +0100

    change state to hooks/ adjust homepage link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,98 +2,75 @@ import { Button } from './Button/Button';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Searchbar } from './Searchbar/Searchbar';
 import { Modal } from './Modal/Modal';
-import { Component } from 'react';
 import { fetchPhotos } from '../components/services/fetchPhotos';
 import { Loader } from './Loader/Loader';
 import { Notify } from 'notiflix';
+import { useState } from 'react';
 
 export const App = () => {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: '',
-      page: 1,
-      images: [],
-      modalFormatSrc: '',
-      isModalOpen: false,
-      showBtn: false,
-      isLoading: false,
-    };
-    this.onPageChange = this.onPageChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onModalClose = this.onModalClose.bind(this);
-  }
+  const [query, setQuery] = useState('');
+  const [isModalOpen, setModal] = useState();
+  const [modalFormatSrc, setModalFormatSrc] = useState('');
+  const [isLoading, setIsLoading] = useState();
+  const [images, setImages] = useState([]);
+  const [page = 1, setPage] = useState();
+  const [showBtn, setButton] = useState();
 
-  onSubmit = evt => {
+  const onSubmit = evt => {
     evt.preventDefault();
-    this.setState({
-      query: evt.target.search.value,
-      isModalOpen: false,
-      modalFormatSrc: '',
-      isLoading: true,
-      images: [],
-    });
-    this.search(evt.target.search.value, this.state.page);
+    const inputValue = evt.target.search.value;
+    setQuery(inputValue);
+    setModal(false);
+    setModalFormatSrc('');
+    setIsLoading(true);
+    setImages([]);
+    search(inputValue, page);
   };
 
-  onPageChange = () => {
-    this.setState({
-      page: this.state.page + 1,
-      isLoading: true,
-    });
-    this.search(this.state.query, this.state.page + 1);
+  const onPageChange = () => {
+    setPage(page + 1);
+    setIsLoading(true);
+    search(query, page + 1);
   };
 
-  onImageClick = src => {
-    const modalFormat = this.state.images.find(
-      el => el.webformatURL === src.target.src
-    );
-    this.setState({
-      isModalOpen: true,
-      modalFormatSrc: modalFormat.largeImageURL,
-    });
+  const onImageClick = src => {
+    const modalFormat = images.find(el => el.webformatURL === src.target.src);
+    setModal(true);
+    setModalFormatSrc(modalFormat.largeImageURL);
   };
 
-  onModalClose = () => {
-    this.setState({ isModalOpen: false });
+  const onModalClose = () => {
+    setModal(false);
   };
 
- const async search(query, page) {
+  const search = async (query, page) => {
     const response = await fetchPhotos(query, page);
-    this.setState(prevState => {
-      return {
-        images: [...prevState.images, ...response.data.hits],
-      };
+    setImages(prevState => {
+      return [...prevState, ...response.data.hits];
     });
     if (response.data.hits.length < 12) {
-      this.setState({ showBtn: false });
+      setButton(false);
     }
     if (response.data.hits.length === 12) {
-      this.setState({ showBtn: true });
+      setButton(true);
     }
     if (response.data.hits.length >= 1 || response.data.hits.length === 0) {
-      this.setState({ isLoading: false });
+      setIsLoading(false);
     }
     if (response.data.hits.length === 0) {
       Notify.failure("Didn't find any matches!");
     }
-  }
+  };
 
-    return (
-      <>
-        <Searchbar onSubmit={this.onSubmit} />
-        <ImageGallery
-          items={this.state.images}
-          handleClick={this.onImageClick}
-        />
-        {this.state.isLoading && <Loader />}
-        {this.state.showBtn && <Button handleClick={this.onPageChange} />}
-        {this.state.isModalOpen && (
-          <Modal
-            largeImageURL={this.state.modalFormatSrc}
-            onClose={this.onModalClose}
-          />
-        )}
-      </>
-    );
-}
+  return (
+    <>
+      <Searchbar onSubmit={onSubmit} />
+      <ImageGallery items={images} handleClick={onImageClick} />
+      {isLoading && <Loader />}
+      {showBtn && <Button handleClick={onPageChange} />}
+      {isModalOpen && (
+        <Modal largeImageURL={modalFormatSrc} onClose={onModalClose} />
+      )}
+    </>
+  );
+};
